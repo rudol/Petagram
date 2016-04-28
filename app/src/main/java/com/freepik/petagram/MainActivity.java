3146345f7b5d5fd697232c16af82920e04549c1b
rudol <rudolruj@gmail.com>
2016-04-28 00:51:38 -0300

    Initial commit

---
+++ app/src/main/java/com/freepik/petagram/MainActivity.java
@@ -0,0 +1,15 @@
+package com.freepik.petagram;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import petagram.freepik.com.petagram.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
